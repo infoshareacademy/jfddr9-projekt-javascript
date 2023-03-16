d118d1bccb2be575b177e8c97b8eabf9f3d00d17
Szymon Lamparski <szymonlamparski@gmail.com>
2023-03-16 13:00:09 +0100

    addToList added

--- Projects-1/todo-list/script.js
+++ Projects-1/todo-list/script.js
@@ -1,2 +1,13 @@
+let button = document.querySelector(".btn");
 
+button.addEventListener("click", addToList);
+
+const todos = [];
+
+function addToList() {
+    let input = document.getElementById("task");
+    let task = input.value;
+    todos.push(task)
+    console.log(todos);
+}
 
