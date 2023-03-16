7267300a44adf8b7755acc914fc2bd113f67e554
Katarzyna Brejna <kb.brejna@gmail.com>
2023-03-16 13:47:18 +0100

    Todo-list complete

--- Projects-1/todo-list/script.js
+++ Projects-1/todo-list/script.js
@@ -2,11 +2,17 @@ let button = document.querySelector("button.btn");
 
 button.addEventListener("click", addToList);
 
-const todos = [];
-
+const todos = []; //ten todos to nazwa tablicy
 function addToList() {
   let input = document.getElementById("task");
   let task = input.value;
   todos.push(task);
+
+  let table = document.querySelector("#todos"); //ten todos to odwołanie do elementu z HTML
+  let newTaskElement = document.createElement("tr");
+  newTaskElement.innerText = task;
+  table.appendChild(newTaskElement);
+  input.value = "";
+
   console.log(todos);
 }
