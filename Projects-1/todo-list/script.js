1fdb6e8a4a39cc81d3e6d29cfb97b7828f7230f8
Szymon Lamparski <szymonlamparski@gmail.com>
2023-03-16 13:22:03 +0100

    creating tr element appending to table

--- Projects-1/todo-list/script.js
+++ Projects-1/todo-list/script.js
@@ -5,9 +5,15 @@ button.addEventListener("click", addToList);
 const todos = [];
 
 function addToList() {
-    let input = document.getElementById("task");
+    let input = document.querySelector("input.form-control");
     let task = input.value;
-    todos.push(task)
-    console.log(todos);
+    todos.push(task);
+
+    let table = document.querySelector("#todos");
+    let newTaskElement = document.createElement("tr");
+    newTaskElement.innerText = task;
+    table.appendChild(newTaskElement);
+
+    console.log(task);
 }
 
