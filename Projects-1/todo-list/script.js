54db8641508a98010cb0cd72fe369d6fcb90d5bc
Bartosz Zagozdon <bart.zagozdon@gmail.com>
2023-03-16 12:54:27 +0100

    First task done

--- Projects-1/todo-list/script.js
+++ Projects-1/todo-list/script.js
@@ -1,2 +1,6 @@
-
-
+let add = () => {
+    let task = document.querySelector('.form-control').value;
+    tBody.insertAdjacentHTML('beforeend', `<tr>${task}</tr>`);
+}
+let tBody = document.querySelector('#todos');
+document.querySelector('.btn').addEventListener('click', add);
