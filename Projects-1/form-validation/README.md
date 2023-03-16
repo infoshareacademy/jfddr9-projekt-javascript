7b0b29248a72d0f05e717752c83f3efda4e086c0
piechotadawid2000@gmail.com <piechotadawid2000@gmail.com>
2023-03-16 15:01:19 +0100

    everything?

--- Projects-1/form-validation/README.md
+++ Projects-1/form-validation/README.md
@@ -3,7 +3,7 @@
 ## Wymagania
 
 1. Jeżeli nie zostały wypełnione wszystkie pola, przycisk `Submit form` powinien być `disabled`
-2. Zasada ogólna - po tym jak użytkownik wypełni dane pole musi nastąpić jego walidacja oraz w przypadku wystąpienia błedu, wyświetlenie komunikatu  
+2. Zasada ogólna - po tym jak użytkownik wypełni dane pole musi nastąpić jego walidacja oraz w przypadku wystąpienia błedu, wyświetlenie komunikatu
 3. `First Name` - imie powinno zawierać od 10 do maksymalnie 100 znaków (wyłącznie liter)
 4. `Last Name` - nazwisko powinno zawierać od 10 do maksymalnie 100 znaków (wyłącznie liter), oraz opcjonalnie jeden
    myślnik dla nazwisk z dwóch członów
@@ -15,4 +15,19 @@
    zweryfikowana czy mieści się w podanym zakresie oraz, czy ma prawidłowy format.
 9. `Agree to terms and conditions` - zgoda musi być zaakceptowana
 10. W przypadku złamania zasad dla danego pola powinien pojawić się komunikat, dla którego pola wystąpił błąd tzn. jeżeli podałem imię, które ma więcej niż 100 znaków powinienem otrzymać komunikat `First name invalid`
-11. [Dla chętnych] możecie dostosować wiadomość błędu dla każdego pola i przypadku złamania walidacji np. `First name too long. Max length is 100 charts.`, które pojawiają się bezpośrednio pod nim 
+11. [Dla chętnych] możecie dostosować wiadomość błędu dla każdego pola i przypadku złamania walidacji np. `First name too long. Max length is 100 charts.`, które pojawiają się bezpośrednio pod nim
+
+12. If not all fields have been filled in, the `Submit form` button should be `disabled`
+13. General rule - after the user fills in a given field, it must be validated and, in the event of an error, a message is displayed
+14. `First Name` - the name should contain from 10 to a maximum of 100 characters (letters only)
+15. `Last Name` - the name should contain from 10 to a maximum of 100 characters (letters only), and optionally one
+    hyphen for names with two members
+16. `Username` - username should contain from 10 to 30 characters (only numbers or letters)
+17. `City` - we create the form only for people who live in Bydgoszcz, so this field should contain 1:1
+    content 'Bydgoszcz'
+18. `State` - name of the province, i.e. `Kujawsko-pomorskie`, `kuj-pom` or `kuj-pomorskie` other values ​​are not correct
+19. `Zip` - zip code for Bydgoszcz, it covers the range from `85-000` to `85-980`. The entered value must be
+    verified that it falls within the given range and that it is in the correct format.
+20. `Agree to terms and conditions` - consent must be accepted
+21. In case of breaking the rules for a given field, a message should appear for which field the error occurred, i.e. if I entered a name that has more than 100 characters, I should receive the message `First name invalid`
+22. [For volunteers] you can customize the error message for each field and case of validation violation, e.g. `First name too long. Max length is 100 charts.` which appear directly below it
--- Projects-1/form-validation/script.js
+++ Projects-1/form-validation/script.js
@@ -1,2 +1,124 @@
+let button = document.querySelector("button.btn");
 
+button.addEventListener("click", submit);
 
+let user = [];
+
+function submit() {
+  firstNameFunction();
+  lastNameFunction();
+  username();
+  city();
+  province();
+}
+
+function firstNameFunction() {
+  let firstNameInput = document.querySelector("input#validationCustom01");
+  let firstName = firstNameInput.value;
+  let lengthFirstName = firstName.length;
+
+  if (
+    /^[a-zA-Z]+$/.test(firstName) &&
+    lengthFirstName > 9 &&
+    lengthFirstName < 101
+  ) {
+    console.log("first name true");
+    user.push(firstName);
+  } else {
+    console.log("first name false");
+  }
+
+  //   let table = document.querySelector("#todos");
+  //   let newTaskElement = document.createElement("tr");
+  //   newTaskElement.innerText = task;
+  //   table.appendChild(newTaskElement);
+  //   input.value = "";
+}
+
+function lastNameFunction() {
+  let lastNameInput = document.querySelector("input#validationCustom02");
+  let lastName = lastNameInput.value;
+  let lengthLastName = lastName.length;
+
+  if (
+    /^[a-zA-Z.-]+$/.test(lastName) &&
+    lengthLastName > 9 &&
+    lengthLastName < 101
+  ) {
+    console.log("last name true");
+    user.push(lastName);
+  } else {
+    console.log("last name false");
+  }
+
+  //   let table = document.querySelector("#todos");
+  //   let newTaskElement = document.createElement("tr");
+  //   newTaskElement.innerText = task;
+  //   table.appendChild(newTaskElement);
+  //   input.value = "";
+}
+
+function username() {
+  let usernameInput = document.querySelector("input#validationCustomUsername");
+  let username = usernameInput.value;
+  let lengthUsername = username.length;
+
+  if (
+    /^[a-zA-Z0-9]+$/.test(username) &&
+    lengthUsername > 9 &&
+    lengthUsername < 31
+  ) {
+    console.log("username true");
+    user.push(username);
+  } else {
+    console.log("username false");
+  }
+
+  //   let table = document.querySelector("#todos");
+  //   let newTaskElement = document.createElement("tr");
+  //   newTaskElement.innerText = task;
+  //   table.appendChild(newTaskElement);
+  //   input.value = "";
+}
+
+function city() {
+  let cityInput = document.querySelector("input#validationCustom03");
+  let city = cityInput.value;
+
+  if (city === "Bydgoszcz") {
+    console.log("city true");
+    user.push(city);
+  } else {
+    console.log("city false");
+  }
+
+  //   let table = document.querySelector("#todos");
+  //   let newTaskElement = document.createElement("tr");
+  //   newTaskElement.innerText = task;
+  //   table.appendChild(newTaskElement);
+  //   input.value = "";
+}
+
+function province() {
+  let provinceInput = document.querySelector("input#validationCustom04");
+  let province = provinceInput.value;
+
+  if (
+    province === "Kujawsko-pomorskie" ||
+    province === "kuj-pom" ||
+    province === "kuj-pomorskie"
+  ) {
+    console.log("province true");
+    user.push(province);
+  } else {
+    console.log("province false");
+  }
+
+  //   let table = document.querySelector("#todos");
+  //   let newTaskElement = document.createElement("tr");
+  //   newTaskElement.innerText = task;
+  //   table.appendChild(newTaskElement);
+  //   input.value = "";
+}
+
+console.log(user);
