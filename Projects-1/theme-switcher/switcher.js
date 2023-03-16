f391ee050af11ca33cf194a3fa6c540af0f1202a
aleksandra-jablonska <aleksandra.ola.jablonski@gmail.com>
2023-03-16 14:51:01 +0100

    clean code

--- Projects-1/theme-switcher/switcher.js
+++ Projects-1/theme-switcher/switcher.js
@@ -2,24 +2,16 @@ let backgroundPage = document.querySelector("body");
 
 let darkButton = document.querySelector(".btnDark");
 
-darkButton.addEventListener("click", changeToDarkMode);
+darkButton.addEventListener("click", () => changeToMode("white", "black"));
 
-function changeToDarkMode(color) {
-  document.body.style.background = "black";
-  document.querySelector(".btnDark").style.borderColor = "white";
-  document.querySelector(".btnLight").style.borderColor = "white";
-  document.querySelector(".btnDark").style.color = "white";
-  document.querySelector(".btnLight").style.color = "white";
+function changeToMode(color, backgroundColor) {
+  document.body.style.background = backgroundColor;
+  document.querySelector(".btnDark").style.borderColor = color;
+  document.querySelector(".btnLight").style.borderColor = color;
+  document.querySelector(".btnDark").style.color = color;
+  document.querySelector(".btnLight").style.color = color;
 }
 
 let lightButton = document.querySelector(".btnLight");
 
-lightButton.addEventListener("click", changeToLightMode);
-
-function changeToLightMode(color) {
-  document.body.style.background = "white";
-  document.querySelector(".btnDark").style.borderColor = "black";
-  document.querySelector(".btnLight").style.borderColor = "black";
-  document.querySelector(".btnDark").style.color = "black";
-  document.querySelector(".btnLight").style.color = "black";
-}
+lightButton.addEventListener("click", () => changeToMode("black", "white"));
