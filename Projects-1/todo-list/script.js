07f7d571cf4d72a71e425702fa923fd7f3f300dc
Katarzyna Brejna <kb.brejna@gmail.com>
2023-03-30 06:54:14 +0200

    Project-1 correction

--- Projects-1/todo-list/script.js
+++ Projects-1/todo-list/script.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 let button = document.querySelector("button.btn")
 
 button.addEventListener("click", addToList)
@@ -20,5 +21,26 @@ function addToList() {
 
 
 
+=======
+let button = document.querySelector(".btn-primary");
+>>>>>>> project-2/KatarzynaBrejna
 
+button.addEventListener("click", addToList);
 
+const todos = [];
+
+function addToList() {
+  let input = document.querySelector("input.form-control");
+  let task = input.value;
+
+  todos.push(task);
+
+  let table = document.querySelector("#todos");
+  let newTaskElement = document.createElement("tr");
+
+  newTaskElement.innerText = task;
+  table.appendChild(newTaskElement);
+  input.value = "";
+
+  console.log(todos);
+}
