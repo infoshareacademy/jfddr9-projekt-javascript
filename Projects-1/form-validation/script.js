dc8b69ffcee7d3888657e0af4d93001f8713659e
Katarzyna Brejna <kb.brejna@gmail.com>
2023-04-11 22:22:47 +0200

    Task 5 - Username validation

--- Projects-1/form-validation/script.js
+++ Projects-1/form-validation/script.js
@@ -1,10 +1,14 @@
 const firstNameInput = document.querySelector("#validationCustom01");
 const lastNameInput = document.querySelector("#validationCustom02");
+const usernameInput = document.querySelector("#validationCustomUsername");
 firstNameInput.addEventListener("change", firstNameValidation);
 lastNameInput.addEventListener("change", lastNameValidation);
+usernameInput.addEventListener("change", usernameValidation);
 
-const MIN_FIRST_NAME_LENGTH = 3;
-const MAX_FIRST_NAME_LENGTH = 25;
+const MIN_NAME_LENGTH = 10;
+const MAX_NAME_LENGTH = 100;
+const MIN_USERNAME_LENGTH = 10;
+const MAX_USERNAME_LENGTH = 30;
 
 function showNameErrorMessage(errorMessage, id) {
   const validationDiv = document.querySelector(id);
@@ -16,7 +20,7 @@ function showNameErrorMessage(errorMessage, id) {
 }
 
 function checkIsOnlyLetters(text) {
-  const regEx = new RegExp(/[a-zA-Z]+/);
+  const regEx = new RegExp(/^[a-zA-Z]+$/);
   return text.match(regEx);
 }
 
@@ -28,7 +32,7 @@ function firstNameValidation() {
   validationDiv.innerHTML = "";
 
   //błąd za mało znaków
-  if (firstNameLength < MIN_FIRST_NAME_LENGTH) {
+  if (firstNameLength < MIN_NAME_LENGTH) {
     showNameErrorMessage(
       "Minimalna liczba znakow to 3",
       "#firstNameValidationMessage"
@@ -36,7 +40,7 @@ function firstNameValidation() {
   }
 
   //błąd za dużo znaków
-  if (firstNameLength > MAX_FIRST_NAME_LENGTH) {
+  if (firstNameLength > MAX_NAME_LENGTH) {
     showNameErrorMessage(
       "Maksymalna liczba znakow to 25",
       "#firstNameValidationMessage"
@@ -52,36 +56,66 @@ function firstNameValidation() {
   }
 }
 
-
 function lastNameValidation() {
-    const lastNameValue = lastNameInput.value;
-    const lastNameLength = lastNameValue.length;
-    const validationDiv = document.querySelector("#lastNameValidationMessage");
-  
-    validationDiv.innerHTML = "";
-  
-    //błąd za mało znaków
-    if (lastNameLength < MIN_FIRST_NAME_LENGTH) {
-      showNameErrorMessage(
-        "Minimalna liczba znakow to 3",
-        "#lastNameValidationMessage"
-      );
-    }
-  
-    //błąd za dużo znaków
-    if (lastNameLength > MAX_FIRST_NAME_LENGTH) {
-      showNameErrorMessage(
-        "Maksymalna liczba znakow to 25",
-        "#lastNameValidationMessage"
-      );
-    }
-  
-    //tylko literki
-    if (!checkIsOnlyLetters(lastNameValue)) {
-      showNameErrorMessage(
-        "Błędne znaki w imieniu",
-        "#lastNameValidationMessage"
-      );
-    }
+  const lastNameValue = lastNameInput.value;
+  const lastNameLength = lastNameValue.length;
+  const validationDiv = document.querySelector("#lastNameValidationMessage");
+
+  validationDiv.innerHTML = "";
+
+  //błąd za mało znaków
+  if (lastNameLength < MIN_NAME_LENGTH) {
+    showNameErrorMessage(
+      "Minimalna liczba znakow to 3",
+      "#lastNameValidationMessage"
+    );
   }
-  
+
+  //błąd za dużo znaków
+  if (lastNameLength > MAX_NAME_LENGTH) {
+    showNameErrorMessage(
+      "Maksymalna liczba znakow to 25",
+      "#lastNameValidationMessage"
+    );
+  }
+
+  //tylko literki
+  if (!checkIsOnlyLetters(lastNameValue)) {
+    showNameErrorMessage(
+      "Błędne znaki w imieniu",
+      "#lastNameValidationMessage"
+    );
+  }
+}
+
+function checkIsLettersOrNumbers(text) {
+  const regEx = new RegExp(/^[a-zA-Z0-9]+$/);
+  return text.match(regEx);
+}
+
+function usernameValidation() {
+  const usernameValue = usernameInput.value;
+  const usernameLength = usernameValue.length;
+  const validationDiv = document.querySelector("#usernameValidationMessage");
+
+  // validationDiv = "";
+
+  if (usernameLength < MIN_USERNAME_LENGTH) {
+    showNameErrorMessage(
+      "Minimalna liczba znaków to 10",
+      "#usernameValidationMessage"
+    );
+  }
+  if (usernameLength > MAX_USERNAME_LENGTH) {
+    showNameErrorMessage(
+      "Maksymalna liczba znaków to 30",
+      "#usernameValidationMessage"
+    );
+  }
+  if (!checkIsLettersOrNumbers(usernameValue)) {
+    showNameErrorMessage(
+      "Błędne znaki w nazwie użytkownika",
+      "#usernameValidationMessage"
+    );
+  }
+}
